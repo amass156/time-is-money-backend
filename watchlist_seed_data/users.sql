406d22c2eb204e6194b52ec7320aa6a441a4bab6
amass156 <massiah156@gmail.com>
2021-09-07 14:35:55 -0400

    backend setup

---
+++ watchlist_seed_data/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(name, username, password) VALUES ('Andre Massiah', 'amassiah', 'password');
+INSERT INTO users(name, username, password) VALUES ('Nelson Mandela', 'freedom', 'courage');
+INSERT INTO users(name, username, password) VALUES ('Rashaad Bilal', 'earnyourleisure', 'finance');
+INSERT INTO users(name, username, password) VALUES ('Chris Johnson', 'wealthsquad', 'passive');
